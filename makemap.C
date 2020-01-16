769b1885286b0ffcc5e2e2975d7cb736ecc07446
John Haggerty <haggerty@bnl.gov>
2020-01-16 11:56:27 -0500

    Change the name of the default map, use the 2.85 cm shifted map

--- makemap.C
+++ makemap.C
@@ -5,7 +5,7 @@
 #include "TFile.h"
 #include "TNtuple.h"
 
-Int_t makemap( std::string filename = "TPC_fld_map_f.table" )
+Int_t makemap( std::string filename = "TPC_fld_map_f_shiftby2p85cm.table" )
 {
   std::ifstream ifs( filename ); 
   std::string line; 
@@ -19,9 +19,9 @@ Int_t makemap( std::string filename = "TPC_fld_map_f.table" )
   // unit vector from the origin to (x,y) 
   double vx,vy;
 
-  std::string rootfile = "sphenix3dcylmap_0.root";
+  std::string rootfile = "sphenix3dmaprhophiz.root";
   TFile *f = new TFile(rootfile.c_str(),"recreate");
-  TNtuple B("B","sPHENIX 3D Field Map 2020.01.14 (Gauss)","rho:phi:z:brho:bphi:bz");
+  TNtuple B("B","sPHENIX 3D Field Map TPC_fld_map_f_shiftby2p85cm 2020.01.16 (Gauss)","rho:phi:z:brho:bphi:bz");
   
   int linenum = 0; 
   while (getline (ifs, line)) { 
